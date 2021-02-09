5110bd8f8430d13f15d585253963b9f215d31cc7
mithileshchellappan <35962266+mithileshchellappan@users.noreply.github.com>
2021-02-09 10:33:07 +0530

    header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {PostCreateComponent} from './posts/post-create/post-create.component';
+import { PostCreateComponent } from './posts/post-create/post-create.component';
+import {MatInputModule} from "@angular/material/input";
+import { MatCardModule } from "@angular/material/card";
+import { MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { HeaderComponent } from './header/header.component';
 @NgModule({
-  declarations: [
-    AppComponent,
-    PostCreateComponent
-  ],
+  declarations: [AppComponent, PostCreateComponent,HeaderComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FormsModule,
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule,
+    MatToolbarModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -3,11 +3,17 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: 'app-post-create',
-    templateUrl : './post-create.component.html'
+    templateUrl : './post-create.component.html',
+    styleUrls:['./post-create.component.css']
     
 })
 export class PostCreateComponent {
+    newPost = 'Test';
+    enteredValue = '';
+    
     onAddPost(){
+        
+        this.newPost = this.enteredValue;
         
     }
 }
