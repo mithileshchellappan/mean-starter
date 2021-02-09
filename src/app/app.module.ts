80058a18138271b27e2111d358895b29b1d9dcc0
mithileshchellappan <35962266+mithileshchellappan@users.noreply.github.com>
2021-02-09 10:54:18 +0530

    ngFor loop

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
-import {MatInputModule} from "@angular/material/input";
-import { MatCardModule } from "@angular/material/card";
-import { MatButtonModule} from '@angular/material/button';
-import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { HeaderComponent } from './header/header.component';
+import { PostListComponent } from './posts/post-list/post-list.component';
+import{MatExpansionModule} from '@angular/material/expansion';
 @NgModule({
-  declarations: [AppComponent, PostCreateComponent,HeaderComponent],
+  declarations: [
+    AppComponent,
+    PostCreateComponent,
+    HeaderComponent,
+    PostListComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -20,7 +27,8 @@ import { HeaderComponent } from './header/header.component';
     MatInputModule,
     MatCardModule,
     MatButtonModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatExpansionModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/posts/post-list/post-list.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-post-list',
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.css'],
+})
+export class PostListComponent {
+  posts = [
+    { title: 'First Post', content: 'This is content 1' },
+    { title: 'Second Post', content: 'This is content 2' },
+    { title: 'Third Post', content: 'This is content 3' },
+  ];
+}
